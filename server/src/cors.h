0469dc9d14a9563f3917b8ea3787b40a616d19fd
root <root@DESKTOP-S3LHK8F>
2024-12-17 02:43:03 +0300

    remove debug print

--- server/src/cors.h
+++ server/src/cors.h
@@ -6,10 +6,6 @@ struct CORSHandler {
     struct context {};
 
     void before_handle(crow::request& req, crow::response& res, context&) {
-
-        std::cerr << "GOVNO" << std::endl;
-        std::cerr << res.body << std::endl;
-
         res.add_header("Access-Control-Allow-Origin", "http://localhost:3000");
         res.add_header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
         res.add_header("Access-Control-Allow-Headers", "Content-Type");
